51e01b3b9978bc1530fa56d8734c015da90085af
Thomas Lau <thomas_0836@qq.com>
2020-05-01 01:28:51 +0800

    fix bug

--- .umirc.ts
+++ .umirc.ts
@@ -11,13 +11,13 @@ export default defineConfig({
     logLevel: 'info',
     defaultSizes: 'parsed', // stat  // gzip
   },
-  base: '/dumi_test/',
+  base: '/qiankun_test/',
   hash: true,
   ignoreMomentLocale: true,
   layout: {},
   nodeModulesTransform: {
     type: 'none',
   },
-  publicPath: '/dumi_test/',
+  publicPath: '/qiankun_test/',
   routes: [{ path: '/', component: '@/pages/index' }],
 });
